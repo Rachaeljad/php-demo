7eeccc553c51a436aad723e98b290e8b0d35bfbd
Rachaeljad <Rachaeljad@gmail.com>
2015-04-02 20:09:38 -0600

    php changes

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+<?php include 'nav.php';?>
+
+ <p>Copyright 2015 -Rachael</p>
+</footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ $userName= "YourName";
+ 
+ $pageID = strtolower($pageTitle);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?php echo $pageTitle;?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel ="stylesheet" href="style.css">
+</head>
+
+<body id="<?php echo $pageID;?>">
+
+<header>
+	<div class="row clearfix">
+
+<a href="index.php"><img class="logo" src="logo.png"/></a>
+
+<?php include 'nav.php';?>
+</div>
+
+</header>
